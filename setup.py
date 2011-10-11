47ef77f7f570f5c19d7062fd10ef17d622b1cfc1
cloomis <cloomis@D-140-142-30-217.dhcp4.washington.edu>
2011-10-11 21:36:06 +0000

    create ecameraICC link _after_ installing

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import distutils
+import distutils.command.install as distInstall
 from distutils.core import setup, Extension
 
 import sdss3tools
@@ -12,14 +13,20 @@ numpy_incl_dir = numpy.get_include()
 # Where we have put numpy.i -- in time this may come from swig or numpy.
 sdss3tools_numpy_incl_dir = os.path.join(os.getenv('SDSS3TOOLS_DIR'), 'python')
 
+class my_install(distInstall.install):
+    def run(self):
+        distInstall.install.run(self)
+        os.symlink(os.path.join(self.install_lib, 'gcameraICC'),
+                   os.path.join(self.install_lib, 'ecameraICC'))
+        
 sdss3tools.setup(
-        description = "ICC to control SDSS3 Alta guide camera",
-        name = "gcameraICC",
-        #packages = ['python/gcameraICC'],
-        ext_modules=[Extension('_alta', ['python/gcameraICC/alta/alta.i'],
-                               swig_opts=['-c++', '-I'+apogee_src_dir, '-I'+sdss3tools_numpy_incl_dir, '-DALTA_STANDALONE'],
-                               include_dirs=[apogee_src_dir, numpy_incl_dir],
-                               define_macros=[('ALTA_STANDALONE', None)],
-                               extra_objects=[os.path.join(apogee_src_dir, '_apogee_net.so')])],
-        )
+    description = "ICC to control SDSS3 Alta guide camera",
+    name = "gcameraICC",
+    ext_modules=[Extension('_alta', ['python/gcameraICC/alta/alta.i'],
+                           swig_opts=['-c++', '-I'+apogee_src_dir, '-I'+sdss3tools_numpy_incl_dir, '-DALTA_STANDALONE'],
+                           include_dirs=[apogee_src_dir, numpy_incl_dir],
+                           define_macros=[('ALTA_STANDALONE', None)],
+                           extra_objects=[os.path.join(apogee_src_dir, '_apogee_net.so')])],
+    cmdclass=dict(install=my_install),
+    )
 
